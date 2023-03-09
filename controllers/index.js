a6611f47e769d55ee49bacc094a121a6fb6d8e30
wahidfebr <wahidfebr@gmail.com>
2023-03-10 04:38:49 +0700

    login done but not yet with session

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
+const {User} = require("../models");
+const {bcrypt} = require("../helpers");
 class Controller {
     static home(req, res) {
         res.render("index");
@@ -5,6 +7,21 @@ class Controller {
 
     static loginForm(req, res) {
         res.render("login-form");
+    }
+
+    static verifyUser(req, res) {
+        const {email, password, saveSession} = req.body;
+        User.findOne({where: {email}})
+            .then(user => {
+                if (!user) throw "User not found";
+                if (!bcrypt.compareSync(password, user.password)) {
+                    throw "Wrong password"
+                }
+
+                if (user.role === "Teacher") res.redirect("/teacher");
+                if (user.role === "Student") res.redirect("/student");
+            })
+            .catch(err => res.send(err))
     }
 
     static registerForm(req, res) {
