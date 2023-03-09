a444b4c72432e32cccd851c15e6b66e0e0d561a2
wahidfebr <wahidfebr@gmail.com>
2023-03-09 17:12:50 +0700

    add seeders Profiles

---
+++ seeders/20230309100957-seed-Profiles.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require("fs");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const profiles = JSON.parse(fs.readFileSync("./data/profiles.json", "utf-8"))
+      .map(el => {
+        el.createdAt = el.updatedAt = new Date();
+        return el;
+      })
+    
+    await queryInterface.bulkInsert("Profiles", profiles);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Profiles");
+  }
+};
