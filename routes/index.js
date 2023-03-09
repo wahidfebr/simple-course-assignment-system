9e52f6cc1a1f4704aa63243edcc0adc93f97cd46
wahidfebr <wahidfebr@gmail.com>
2023-03-10 00:27:25 +0700

    init simple routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const Controller = require("../controllers");
 const router = require("express").Router();
 
+const routerLogin = require("./login");
+const routerRegister = require("./register");
+const routerStudent = require("./student");
+const routerTeacher = require("./teacher");
+
+
 router.get("/", Controller.home);
+
+router.use("/login", routerLogin);
+router.use("/register", routerRegister);
+router.use("/student", routerStudent);
+router.use("/teacher", routerTeacher);
 
 module.exports = router;
