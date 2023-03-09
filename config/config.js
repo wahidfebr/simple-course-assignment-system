5d65f02c88f5a3e081674d8b3acd34e87d64e4fa
Ryusei Kasagawa <sgtryu1@gmail.com>
2023-03-09 13:48:27 +0700

    Test

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_NAME,
+    "host": process.env.DEV_DB_HOSTNAME,
+    "port": process.env.DEV_DB_PORT,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
